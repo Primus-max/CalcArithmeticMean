bdb4743338abb4368f62174738f2c52cbf822aa7
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-03 20:31:56 +0300

    Добавьте файлы проекта.

---
+++ CalcArithmeticMean.cpp
@@ -0,0 +1,27 @@
+﻿// Найти среднее арифметическое всех целых чисел от 1 до 1000
+
+#include <iostream>
+#include<windows.h>
+using namespace std;
+
+
+int main()
+{
+    SetConsoleCP(12510);
+    SetConsoleOutputCP(1251);
+
+    int border = 1000,
+    accum = 0 ,
+    result;
+
+    // Вычисление суммы целых чисел от a до 500
+    for (int i = 1; i <= border; ++i) 
+    {
+        accum += i;        
+    }
+
+    result = accum / border;
+
+    cout << "Среднее арифметическое от 1 до 1000" << " = " << result << endl;
+
+}
